4edc3ad0b90a562d94e65247c227015a37cc9f2f
David <david@keboola.com>
2020-05-27 15:39:13 +0200

    UPDATE better logging, retry policy, unit tests

--- component_config/component_long_description.md
+++ component_config/component_long_description.md
@@ -1,1 +1,8 @@
-Download bank statements from CSOB CEB service. Supports [TXT sepa format](https://www.csob.cz/portal/documents/10710/1927786/format-txt-sepa.pdf) bank statements. These need to be enabled in the CEB UI.
+Download bank statements from CSOB CEB service. Supports [TXT format](https://www.csob.cz/portal/documents/10710/1927786/ceb-vypisy-format-txt.pdf) bank statements. These need to be enabled in the CEB UI.
+
+**Prerequisites:**
+
+- enable the CSOB CEB service
+- get the authentication certificate
+- register the certificate in the portal
+- configure the CSOB service - enable bank statements via API in the `TXT (BB-TXT)` [format](https://www.csob.cz/portal/documents/10710/1927786/ceb-vypisy-format-txt.pdf).
---
+++ tests/ceb/test_client.py
@@ -0,0 +1,33 @@
+import os
+import unittest
+from unittest.mock import patch
+
+from requests import Session, Response
+
+from ceb.client import Client, ClientException
+
+
+class TestClient(unittest.TestCase):
+
+    @patch.object(Session, 'post')
+    @patch("ceb.client.Client._CEB_SERVICE_WSDL", '../../cebbc-wsdl/CEBBCWS.wsdl')
+    @patch("ceb.client.Client.MAX_RETRIES", 0)
+    def test_503_response_invalid_xml_raises_client_exception(self, mock_post):
+        mock_resp = Response()
+        # set mock response
+        mock_resp.status_code = 503
+        mock_resp._content = 'TextResp'
+        mock_resp.headers = {'Content-Type': 'text/xml'}
+        mock_post.return_value = mock_resp
+
+        cl = Client(1234, os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                                       'resources', 'cert.pem'), base_url='https://example.com', debug=True,
+                    max_retries=0, rate_limit_interval=0)
+
+        with self.assertRaises(ClientException) as er:
+            cl.get_download_file_list()
+        print(er.exception)
+
+
+if __name__ == "__main__":
+    unittest.main()
